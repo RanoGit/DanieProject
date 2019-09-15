f796e1605dbb5bcf0d9d9f58f10bd2f6cadf372f
RanoGit <40146649+RanoGit@users.noreply.github.com>
2019-09-14 22:47:40 -0500

    HashMap using Iterator

---
+++ IterateHashMapIterator.java
@@ -0,0 +1,22 @@
+package com.dani.practiceSample.map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class IterateHashMapIterator {
+	
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("key1", "value1");
+		map.put("key2", "value2");
+		Iterator<Entry<String, String>> it = map.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, String> pair = (Map.Entry<String, String>) it.next();
+			System.out.println(pair.getKey() + " = " + pair.getValue());
+		}
+	}
+
+
+}
