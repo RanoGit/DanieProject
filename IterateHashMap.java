99a72908901f0809fa7ee0f428fca0470d94ce33
RanoGit <40146649+RanoGit@users.noreply.github.com>
2019-09-14 22:30:23 -0500

    Hash map using for each

---
+++ IterateHashMap.java
@@ -0,0 +1,17 @@
+package com.dani.practiceSample.map;
+/*
+ * For each is available in Java versions 1.5 +
+ * Hash Map
+ */
+import java.util.HashMap;
+import java.util.Map;
+public class IterateHashMap {
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("key1", "value1");
+		map.put("key2", "value2");
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+		    System.out.println(entry.getKey() + " = " + entry.getValue());
+		}
+	}
+}
